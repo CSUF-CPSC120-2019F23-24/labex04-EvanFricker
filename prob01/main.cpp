6b8cb8a2cb49b2778750b4c73fc1d8be4f8aaba1
EvanFricker <frickere@csu.fullerton.edu>
2019-09-14 13:19:28 -0700

    All 5 TODOS are complete

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -8,7 +8,7 @@ int main()
   // then print both the char and the short
 
   char my_char1 = 'J';
-  short my_short1 = ;
+  short my_short1 = static_cast<int>(my_char1);
   std::cout << "The ASCII value for " << my_char1 << " is " << my_short1 << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
@@ -16,20 +16,28 @@ int main()
   // TODO#2: add one to the character to see what gets printed out
 
   char my_char2 = 'C';
-  my_char2 = ;
+  my_char2 = my_char2 + 1 ;
   std::cout << "Char1: " << my_char2 << std::endl;
 
-  std::cout << std::endl; // blank line to separate output from the different exercises
+  std::cout << std::endl;
+
+  // blank line to separate output from the different exercises
 
   // TODO#3: again add one to the character to see what gets printed out
   // and add a comment explaining why the value of the character is no
   // longer alphabetic
 
+  // The reason that the number is no longer alphabetic is that z is the last
+  // letter, but there are still more ASCII characters, so it goes to the next
+  // one on the list
+
   char my_char3 = 'z';
-  my_char3 = ;
+  my_char3 = my_char3 + 1;
   std::cout << "Char2: " << my_char3 << std::endl;
 
-  std::cout << std::endl; // blank line to separate output from the different exercises
+  std::cout << std::endl;
+
+  // blank line to separate output from the different exercises
 
   // TODO#4: print each number, then add one to each number and print it again
   // also add a comment to explain why my_short2 value became negative after
@@ -37,39 +45,49 @@ int main()
   // note: std::dec and std::hex indicate the format of the output, decimal
   // versus hexadecimal
 
+  // The reason that short 2 became negative and int 2 did not is because short 2
+  // ran out of numbers and looped back around to the negatives, but int 2 still
+  // had more numbers it could accept, so it kept going
+
   short my_short2 = 32767; // maximum value for a short
   int my_int2 = my_short2;
 
   std::cout << "Short (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_short2 << std::hex << " (0x" << my_short2 << ')';
-  my_short2 = ;
+  my_short2 = my_short2 + 1;
   std::cout << ", after adding 1: " << std::dec << my_short2
     << std::hex << " (0x" << my_short2 << ")\n";
 
   std::cout << "Int (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_int2 << std::hex << " (0x" << my_int2 << ')';
-  my_int2 = ;
+  my_int2 = my_int2 + 1;
   std::cout << ", after adding 1: " << std::dec << my_int2
     << std::hex << " (0x" << my_int2 << ")\n";
 
-  std::cout << std::endl; // blank line to separate output from the different exercises
+  std::cout << std::endl;
+
+  // blank line to separate output from the different exercises
 
   // TODO#5: print each number, then add one to each number and print it again
   // also add a comment to explain why my_short2 value became negative after
   // adding one, but my_int2 did not
+
+  // The reason that short 3 became 0 and int 3 did not is because the
+  // space set aside for a short was reached and overflowed back to 0 becase it
+  // was unsigned, but int 3 still had more room, so it did not overflow
 
   unsigned short my_short3 = 65535; // maximum value for an unsigned short
   int my_int3 = my_short3;
 
   std::cout << "Unsigned short (" << sizeof(my_short3) << " bytes): "
     << std::dec << my_short3 << std::hex << " (0x" << my_short3 << ')';
-  my_short3 = ;
+  my_short3 = my_short3 + 1;
   std::cout << ", after adding 1: " << std::dec << my_short3
     << std::hex << " (0x" << my_short3 << ")\n";
 
   std::cout << "Same value as int (" << sizeof(my_int3) << " bytes): "
     << std::dec << my_int3 << std::hex << " (0x" << my_int3 << ')';
-  my_int3 = ;
+  my_int3 = my_int3 + 1;
   std::cout << ", after adding 1: " << std::dec << my_int3
     << std::hex << " (0x" << my_int3 << ")\n";
 
