59afaf1a8bf4e73a638ccfa0563d15ea1fcf57d8
EvanFricker <frickere@csu.fullerton.edu>
2019-09-14 14:01:20 -0700

    Made the program display the correct prediction

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,32 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+
+  // Defining my constants and variables
+
+  const double INCREASE = 0.18;
+  const long LAST_YEAR_SALES = 2103419277;
+  double Sales_Increase;
+  long Predicted_Sales;
+
+  // Displaying the sales from last year
+
+  std::cout << "Last years sales were $" << LAST_YEAR_SALES << '\n';
+
+  // Calculating this years predicted increase in sales
+
+  Sales_Increase = INCREASE * LAST_YEAR_SALES;
+
+  // Calculating the overall predicted sales
+
+  Predicted_Sales = Sales_Increase + LAST_YEAR_SALES;
+
+  // Displaying the predicted sales
+
+  std::cout << "This years predicted sales are $" << Predicted_Sales << '\n';
+
+  return 0;
+}
